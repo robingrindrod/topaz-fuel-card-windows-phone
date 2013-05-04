f03a046ec140b3bb40f4d0065bcfaa3e2043f9aa
Robin Grindrod <robingrindrod@live.ie>
2013-05-04 14:34:38 +0100

    Created Topaz Fuel Card project.

---
+++ Topaz Fuel Card/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Topaz_Fuel_Card.Resources;
+
+namespace Topaz_Fuel_Card
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
